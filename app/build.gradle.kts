2fc3060d9913874122a85ad4af670deb99f10cbf
tg3454 <tnsstuduo@gmail.com>
2026-09-13 22:16:55 +0530

    fixed one error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.gms.play.services.mlkit.barcode.scanning)
 
     // Networking
     implementation(libs.retrofit)
